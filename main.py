297ab144788a70525cb8f6d3c39060b328b5c336
litwor1 <rzepeckapaulina@gmail.com>
2025-12-27 14:29:27 +0100

    Initial commit - includes work from last labs only, more to go..

---
+++ main.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI
+import requests
+import sqlite3
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/sum")
+def sum_ints(x: int = 0, y: int = 10):
+    return x + y
+
+
+@app.get("/geocode")
+def geocode(lat: float, lon: float):
+    url = f"https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat={lat}&lon={lon}"
+    headers = {"User-Agent": "PaulinaISSI/1.0"}
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+
+@app.get('/movies')
+def get_movies():
+    with sqlite3.connect('movies.db') as db:
+        output = []
+        cursor = db.cursor()
+        cursor.execute('SELECT * FROM movies')
+        for row in cursor:
+            movie = {'id': f'{row[0]}', 'title': f'{row[1]}', 'year': f'{row[2]}', 'actors': f'{row[3]}', }
+            output.append(movie)
+        return output
+
+
+@app.get('/movies/{movie_id}')
+def get_single_movie(movie_id: int):
+    with sqlite3.connect('movies.db') as db:
+        cursor = db.cursor()
+        row = cursor.execute('SELECT * FROM movies WHERE id = ?', (movie_id,)).fetchone()
+
+        if row:
+            movie = {
+                'id': row[0],
+                'title': row[1],
+                'year': row[2],
+                'actors': row[3]
+            }
+        else:
+            movie = {'message': 'Movie not found'}
+
+    return movie
